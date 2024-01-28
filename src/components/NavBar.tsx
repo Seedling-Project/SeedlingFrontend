82add485ecc1e559892631a259f5f3393515396e
zeapsu <andryypaez@gmail.com>
2024-01-27 22:49:35 -0800

    fix: navbar component
    not done yet but good enough for now :)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
       {/* Flex container for overall navbar */}
       <div className="flex justify-center w-full">
         {/* Flex container for centered content with limited width */}
-        <div className="flex justify-between items-center w-2/3">
+        <div className="flex justify-between items-center w-5/6">
           <div className="navbar-start">
             <div className="flex items-center space-x-2">
               {" "}
@@ -20,15 +20,17 @@ const Navbar: React.FC = () => {
                 <img
                   src="/final_logo.png"
                   alt="Logo"
-                  className="h-8 w-8 sm:h-10 sm:w-10 md:h-12 md:w-12 lg:h-14 lg:w-14 xl:h-16 xl:w-16 2xl:h-20 2xl:w-20"
+                  className="sm:h-10 sm:w-10 md:h-12 md:w-12 lg:h-14 lg:w-14 xl:h-16 xl:w-16 2xl:h-20 2xl:w-20"
                 />
               </a>{" "}
               {/* Your image */}
-              <span className="text-xl">Seedling Education</span>{" "}
+              <span className="navbar-center text-xl">
+                Seedling Education
+              </span>{" "}
               {/* Text next to the image */}
             </div>
           </div>
-          <div className="navbar-center">
+          <div className="navbar-end w-2/3">
             <ul className="menu menu-horizontal p-0 space-x-1 text-xs">
               <li>
                 <a href="Transfer" className="font-bold">
@@ -57,16 +59,16 @@ const Navbar: React.FC = () => {
               </li>
             </ul>
           </div>
-          <div className="navbar-end">
-            <div className="form-control"></div>
-            {/* <div className="relative text-gray-400 focus-within:text-gray-600 w-full lg:w-auto"> */}
-            {/*   <input */}
-            {/*     type="text" */}
-            {/*     placeholder="Search" */}
-            {/*     className="input input-bordered pl-10" */}
-            {/*   /> */}
-            {/* </div> */}
-          </div>
+          {/* <div className="navbar-end"> */}
+          {/*   <div className="form-control"></div> */}
+          {/* <div className="relative text-gray-400 focus-within:text-gray-600 w-full lg:w-auto"> */}
+          {/*   <input */}
+          {/*     type="text" */}
+          {/*     placeholder="Search" */}
+          {/*     className="input input-bordered pl-10" */}
+          {/*   /> */}
+          {/* </div> */}
+          {/* </div> */}
         </div>
       </div>
     </div>
