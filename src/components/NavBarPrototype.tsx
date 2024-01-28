9c6af27dc10de820a7602c79c95ab7f782db3ee2
zeapsu <andryypaez@gmail.com>
2024-01-28 14:10:07 -0800

    add: setup new navbar component

--- src/components/NavBarPrototype.tsx
+++ src/components/NavBarPrototype.tsx
@@ -1,56 +1,78 @@
-import React from "react";
+import { Navlink } from "react-router-dom";
 
-const NavBarPrototype = () => {
+const Prototype = () => {
   return (
-    <nav
-      style={{ padding: "20px" }}
-      className="navbar top-0 left-0 right-0 z-50 bg-base-100 fixed p-4 md:flex md:delil-antara md:item-center"
-    >
-      {/*Logo*/}
-      <div className="navbar-start text-white text-xl font-bold">
-        <img
-          src="/final_logo.png"
-          alt="Logo"
-          className="h-8 w-8 sm:h-10 sm:w-10 md:h-12 md:w-12 lg:h-14 lg:w-14 xl:h-16 xl:w-16 2xl:h-20 2xl:w-20"
-        />
-      </div>
-
-      {/*Menu Hamburg*/}
-      <div className="md:hidden">
-        <button className="text-white">
-          <svg
-            className="h-6 w-6 filler"
-            xmlns="https://www.w3.org/TR/2018/CR-SVG2-20181004/"
-            viewBox="0 0 24 24"
+    <div className="navbar p-[20px] bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <way
-              fillRule="none"
-              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-8 2a2 2 0 100-4 2 2 0 000 4zM5 12a2 2 0 114 0 2 2 0 0 2 0 0 1 2 0 1 01-40z"
+            <li>
+              <a>Transfer</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>Curriculum</a>
+            </li>
+            <li>
+              <a>Research Opportunities</a>
+            </li>
+            <li>
+              <a>Other Opportunities</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center">
+        <a href="/">
+          <img
+            src="/final_logo.png"
+            alt="Logo"
+            className="sm:h-10 sm:w-10 md:h-12 md:w-12 lg:h-14 lg:w-14 xl:h-16 xl:w-16 2xl:h-20 2xl:w-20"
+          />
+        </a>{" "}
+        <a className="text-xl">Seedling Education</a>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
         </button>
       </div>
-
-      {/* Navigation links */}
-      <div className="navbar-center hidden md:flex space-x-4">
-        <a href="Transfer" className="font-bold">
-          Transfer
-        </a>
-        <a href="Blog" className="font-bold">
-          Blog
-        </a>
-        <a href="Curriculum" className="font-bold">
-          Curriculum
-        </a>
-        <a href="Research" className="font-bold">
-          Research Opportunities
-        </a>
-        <a href="Miscellaneous" className="font-bold">
-          Miscellaneous Opportunities
-        </a>
-      </div>
-    </nav>
+    </div>
   );
 };
 
-export default NavBarPrototype;
+export default Prototype;
