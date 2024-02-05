3164e069351f0c1f9b45e5335ba697b214a35b7c
Abdulahady <abdulwork101@gmail.com>
2024-02-04 16:01:19 -0800

    Fixed navbar spaced out elements. mobile responsive.

--- src/components/NavBarPrototype.tsx
+++ src/components/NavBarPrototype.tsx
@@ -6,10 +6,10 @@ import Research from "./Research";
 const Prototype = () => {
   return (
   <><>
-  <div className="navbar bg-base-100 flex justify-center items-center">
-  <div className="navbar-start  lg:justify-start flex justify-center items-center">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+  <div className="navbar bg-base-100 flex justify-evenly items-center w-full">
+  <div className="navbar-start  lg:justify-start flex justify-around items-center w-full">
+    <div className="dropdown mr-100px">
+      <div tabIndex={0} role="button" className="btn  btn-ghost margin lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
@@ -23,7 +23,7 @@ const Prototype = () => {
 
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl lg:text-center hidden md:unhidden lg:block" >Seedling Education</a> 
+    <a className="btn btn-ghost text-xl lg:text-center hidden md:unhidden :block" >Seedling Education</a> 
 
     <a href="/" className = "min-w-24 min-h-24 w-24 h-24 ">
                 <img
